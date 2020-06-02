fe3a7e6c9719f2a18ca5197bcd57d23bf768d17b
Gabriel Davi <gdavi.gd@gmail.com>
2020-06-02 16:00:04 -0300

    Adding route to create new items

--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,48 @@
-import express from 'express'; 
-
+import express from "express";
+import connection from "./database/connection";
+import knex from "knex";
 const routes = express.Router();
 
-routes.get("/users", (req, res) => {
-  return res.json({ message: "hello" });
+routes.get("/items", async (req, res) => {
+  const items = await connection("items").select("*");
+  const serializedItems = items.map((item) => {
+    return {
+      title: item.title,
+      image_url: `http://localhost:3333/uploads/${item.image}`,
+    };
+  });
+  return res.json(serializedItems);
+});
+
+routes.post("/points", async (req, res) => {
+  const {
+    name,
+    email,
+    whatsapp,
+    latitude,
+    longitude,
+    city,
+    uf,
+    items,
+  } = req.body;
+
+  try {
+    await connection("points").insert({
+      image: "image-fake",
+      name,
+      email,
+      whatsapp,
+      latitude,
+      longitude,
+      city,
+      uf,
+    });
+
+    return res.json({ sucess: true });
+  } catch (error) {
+    console.log(error);
+    return res.status(400).json({ error: error.message || error });
+  }
 });
 
 export default routes;
