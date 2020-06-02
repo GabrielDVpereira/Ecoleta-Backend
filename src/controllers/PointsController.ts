07f51738220a45ee475659574352bef204ce1d80
Gabriel Davi <gdavi.gd@gmail.com>
2020-06-02 18:19:51 -0300

    Finish point controller

--- src/controllers/PointsController.ts
+++ src/controllers/PointsController.ts
@@ -38,10 +38,48 @@ class PointsController {
       });
 
       await trx("point_items").insert(pointItems);
+
+      await trx.commit();
       return res.status(201).json({ ...point, id: point_id });
+    } catch (error) {
+      trx.rollback();
+      return res.status(400).json({ error: error.message || error });
+    }
+  }
+
+  async show(req: Request, res: Response) {
+    const { id } = req.params;
+    try {
+      const [point] = await connection("points").where("id", id).select("*");
+
+      if (!point) res.status(404).json({ error: "point not found" });
+
+      const items = await connection("items")
+        .join("point_items", "items.id", "=", "point_items.item_id")
+        .where("point_items.point_id", id)
+        .select("items.title"); // bringing all items that has point id passsed through params
+
+      return res.json({ point, items });
     } catch (error) {
       return res.status(400).json({ error: error.message || error });
     }
+  }
+
+  async index(req: Request, res: Response) {
+    const { city, uf, items } = req.query;
+
+    const parseItems = String(items)
+      .split(",")
+      .map((item) => Number(item.trim()));
+
+    const points = await connection("points")
+      .join("point_items", "points.id", "=", "point_items.point_id")
+      .whereIn("point_items.item_id", parseItems)
+      .where("city", String(city))
+      .where("uf", String(uf))
+      .distinct()
+      .select("points.*");
+    return res.json(points);
   }
 }
 
